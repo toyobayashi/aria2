ce7e2871a2b886f2fa66845ac691e5228320a1cc
toyobayashi <lifenglin314@outlook.com>
2020-04-22 22:27:58 +0800

    fix Status.bittorrent type

--- src/interfaces/Status.ts
+++ src/interfaces/Status.ts
@@ -81,13 +81,13 @@ export interface Status {
     /** List of lists of announce URIs. If the torrent contains `announce` and no `announce-list`, `announce` is converted to the `announce-list` format. */
     announceList: string[][]
     /** The comment of the torrent. `comment.utf-8` is used if available. */
-    comment: string
+    comment?: string
     /** The creation time of the torrent. The value is an integer since the epoch, measured in seconds. */
-    creationDate: number
+    creationDate?: number
     /** File mode of the torrent. The value is either `single` or `multi`. */
-    mode: string
+    mode?: string
     /** Struct which contains data from Info dictionary. It contains following keys. */
-    info: {
+    info?: {
       /** name in info dictionary. `name.utf-8` is used if available. */
       name: string
     }
