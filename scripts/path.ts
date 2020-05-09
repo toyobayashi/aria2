a207e2463e2f12f4eed37baefb48983782fd8606
toyobayashi <lifenglin314@outlook.com>
2020-05-09 12:28:59 +0800

    use @tybys/tsgo

--- scripts/path.ts
+++
@@ -1,6 +0,0 @@
-import { join, isAbsolute } from 'path'
-
-export default (...args: string[]): string => {
-  if (args.length === 0) return join(__dirname, '..')
-  return isAbsolute(args[0]) ? join(...args) : join(__dirname, '..', ...args)
-}
